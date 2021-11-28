6422a54a76975634d3e9464e7dcd4071918b90ac
OthmaneID <othmanid5@users.noreply.github.com>
2021-11-28 20:54:52 +0100

    'commit'

---
+++ ecom/src/components/nav.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+
+
+const Nav =()=>{
+    return(
+        <nav>
+            <h2>React Ecom</h2>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+
+
+export default Nav;
