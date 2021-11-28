494e75615ca7e2cdd5c41378b72bb8713eac4370
OthmaneID <othmanid5@users.noreply.github.com>
2021-11-28 21:38:16 +0100

    commit

--- ecom/src/components/nav.js
+++ ecom/src/components/nav.js
@@ -1,19 +1,28 @@
-import React from 'react'
+import React from "react";
 
-
-
-const Nav =()=>{
-    return(
-        <nav>
-            <h2>React Ecom</h2>
+const Nav = () => {
+  return (
+    <nav>
+      
+      <div class="custom-shape-divider-top-1638130835">
+        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+          <path
+            d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z"
+            class="shape-fill"
+          ></path>
+        </svg>
+      </div>
+      <div className="nav-containt">
+            <h2> <span>React Ecom</span></h2>
             <ul>
                 <li>Home</li>
                 <li>About</li>
                 <li>Contact</li>
             </ul>
-        </nav>
-    )
-}
-
+      </div>
+        
+    </nav>
+  );
+};
 
 export default Nav;
