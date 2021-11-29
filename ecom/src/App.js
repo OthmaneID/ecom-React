dff91a1a0907b8cb79d38679b997da0b59eac184
OthmaneID <othmanid5@users.noreply.github.com>
2021-11-29 22:42:34 +0100

    commit

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Nav from "./components/nav";
+import Body from "./components/Body";
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      
+      <Body/>
     </div>
   );
 }
---
+++ ecom/src/components/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Body=()=>{
+    return(
+        <div className="body-section">
+
+        </div>
+    );
+}
+
+export default Body;
