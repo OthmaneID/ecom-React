dfc16be9c7ada1051f99fe1535378189f4555b8c
OthmaneID <othmanid5@users.noreply.github.com>
2021-11-29 12:44:13 +0100

    'commit'

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
-import Nav from './components/nav';
-
+import "./App.css";
+import Nav from "./components/nav";
+import Particles from "react-tsparticles";
 
 function App() {
+  const particlesInit = (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
   return (
     <div className="App">
-     <Nav/>
+      <Nav />
+      <div className="body-section">
+        
+      </div>
     </div>
   );
 }
-
 export default App;
